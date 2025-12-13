fb07726f522ca1fc8b12df08e351d8dca803bc19
gabrielbragadev <gabrielmarcosvasconcelos44@gmail.com>
2025-12-13 12:19:43 -0300

    Implemented requirements & reservations post

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+from models.reservation import Reservation
+from datetime import datetime
+
+
+reservations = []
+app = Flask(__name__)
+id = 1
+
+
+@app.route("/reservations", methods=["POST"])
+def create_reservation():
+    global id
+    data = request.get_json()
+    initial_data_time = data["initial_time"]
+    final_data_time = data["final_time"]
+    initial_date_time = datetime.strptime(initial_data_time, "%Y-%m-%dT%H:%M:%S")
+    final_date_time = datetime.strptime(final_data_time, "%Y-%m-%dT%H:%M:%S")
+    for r in reservations:
+        if data["table_number"] == r.table_number:
+            if (
+                initial_date_time <= r.final_time
+                and initial_date_time >= r.initial_time
+            ):
+                return jsonify({"message": "Horário já reservado!"}), 404
+    new_reservation = Reservation(
+        client_name=data["client_name"],
+        table_number=data["table_number"],
+        initial_time=initial_date_time,
+        final_time=final_date_time,
+        id_reservation=id,
+    )
+    reservations.append(new_reservation)
+    id += 1
+    return jsonify({"message": "Reserva feita com sucesso"}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ models/reservation.py
@@ -0,0 +1,7 @@
+class Reservation:
+    def __init__(self, client_name, table_number, initial_time, final_time, id_reservation):
+        self.client_name = client_name
+        self.table_number = table_number
+        self.initial_time = initial_time
+        self.final_time = final_time
+        self.id_reservation = id_reservation
