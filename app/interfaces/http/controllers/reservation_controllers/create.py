1e4496dab045523e63673e32a63db1757aa7a7ec
gabrielbragadev <gabrielmarcosvasconcelos44@gmail.com>
2026-01-24 23:11:28 -0300

    Removing unused imports from the create reservation controller.

--- app/interfaces/http/controllers/reservation_controllers/create.py
+++ app/interfaces/http/controllers/reservation_controllers/create.py
@@ -1,29 +1,12 @@
-from flask import request, jsonify
+from flask import jsonify
 from flask_login import login_required
-from app.application.commands.reservation.create_reservation_command import (
-    CreateReservationCommand,
-)
-from app.application.dtos.reservation.create_reservation_dto import CreateReservationDTO
-from app.infrastructure.persistence.sqlalchemy.unit_of_work import SqlAlchemyUnitOfWork
-from app.infrastructure.persistence.sqlalchemy.table_repository import TableRepository
-from app.domain.services.reservation.reservation_conflict_checker import (
-    ReservationConflictChecker,
-)
-from app.domain.services.reservation.reservation_table_provider import (
-    ReservationTableProvider,
-)
-from app.infrastructure.extensions import db
 
 from app.application.services.reservation.create_reservation_service import (
     CreateReservationService,
 )
-from app.interfaces.http.schemas.reservation.reservation_create_schema import (
-    ReservationCreateSchema,
-)
+
 from app.interfaces.http.controllers.reservation_controllers import reservations_bp
-from app.infrastructure.persistence.sqlalchemy.reservation_repository import (
-    ReservationRepository,
-)
+
 from app.interfaces.http.controllers.reservation_controllers.factories.create_factory import (
     create_factory,
 )
